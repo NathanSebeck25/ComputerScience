1996fd637eb20073cdb41806e134e0a5f5dc1f2e
NathanSebeck25 <pikana7e@gmail.com>
2024-09-24 15:21:04 -0500

    animal project

---
+++ Assignments/Animalsurvay.py
@@ -0,0 +1,6 @@
+user_input1 = input("What is your favorite animal?\n:  ")
+user_input2 = input("why is it your favorite animal?\n: ")
+user_input3 = habitat = input ("where do they live?\n: ")
+user_input4 = color = input ("what color are they?\n: ")
+user_input5 = other_reasons = input("any other reasons?\n: ")
+print( "your favorite animal is "+ user_input1 +" because " + user_input2 + "." + " They live in " + habitat + "." + " they are " + color + ". " + other_reasons + " " +"Are other reasons why" + user_input1  +" is your favorite animal.")
---
+++ Unit 2 - Functions and Variables/practice_sept-24.py
@@ -0,0 +1,16 @@
+#my_variable1 = 4 #declaration-Create
+#pi = 3.1415926535
+#print(my_variable1)
+#my_variable = 20 * my_variable1 #Assignment-Modify
+#print(my_variable)
+
+#concatination = addition of strings together
+#paramiters and conc
+#favorite_animal1 = "gozira"
+#print("Nathan's favorite animal is " + favorite_animal1)
+
+##um= int(num)
+#int(num * 10)
+user_input4 = color = input ("what color are they?\n: ")
+
+print(color)
