2fcf9b255d30794f0c0656bd8ec8a0e0cbb24f5f
NathanSebeck25 <pikana7e@gmail.com>
2024-09-25 15:19:54 -0500

    progress on data types

--- Assignments/Animalsurvay.py
+++ Assignments/Animalsurvay.py
@@ -1,6 +1,7 @@
-user_input1 = input("What is your favorite animal?\n:  ")
-user_input2 = input("why is it your favorite animal?\n: ")
-user_input3 = habitat = input ("where do they live?\n: ")
-user_input4 = color = input ("what color are they?\n: ")
-user_input5 = other_reasons = input("any other reasons? (If no then press enter)\n: ")
+user_input1 = input("What is your favorite animal?\n:  ") # this is a question asking what someones favorite animal is 
+user_input2 = input("why is it your favorite animal?\n: ") # this is a question asking about someones favorite animal 
+user_input3 = habitat = input ("where do they live?\n: ") # this is a question asking about where there animal lives 
+user_input4 = color = input ("what color are they?\n: ") # this is a question asking about what coloer someone favorite animal is 
+user_input5 = other_reasons = input("any other reasons? (If no then press enter)\n: ") #
 print( "your favorite animal is "+ user_input1 +" because " + user_input2 + "." + " They live in " + habitat + "." + " they are " + color + ". " + other_reasons + " " +"Are the reasons why" + " " + user_input1  +" is your favorite animal.")
+# the code above is a summary of the above questions 
