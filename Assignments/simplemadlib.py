f657ad2dde7aafc80d753f25e11153f57e12a1a9
NathanSebeck25 <pikana7e@gmail.com>
2024-09-26 15:19:26 -0500

    Madlib Project

---
+++ Assignments/simplemadlib.py
@@ -0,0 +1,13 @@
+#long ago in a [adj1] [noun1].
+#[noun2] was [adj2] a feild when [name1] found a [nounx].
+# [name1] sold [nounx] it was worth a fortune.
+#and [name1] was able to buy a new [nouny].
+
+adj1 = input("type a adjective\n: ")
+noun1 = input("type a noun\n: ")
+noun2 = input("type a noun\n: ")
+adj2 = input("type a adjective\n: ")
+Name1 = input("type a name\n: ")
+nounx = input("type a noun\n: ")
+nouny = input("type a noun\n:")
+print("long ago in a " + adj1 + " " + noun1 + "." +" " + noun2 + " " + "was"+ " " + adj2 +"a feild when" + " " + Name1 + " " + "found a" + " " + nounx + "." + " " + " " + nounx + " " + "it was worth a fortune and" + " " + Name1 + " " + "was able to buy a new" + " " + nouny + ".")
