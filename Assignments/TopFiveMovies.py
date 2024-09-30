4fdf251a6bcabcb3e801a4c4b5454053d25c7d7a
NathanSebeck25 <pikana7e@gmail.com>
2024-09-30 14:24:29 -0500

    top five movies

---
+++ Assignments/TopFiveMovies.py
@@ -0,0 +1,16 @@
+def top_5_Movies():
+    print("Nathan's top 5 movies")
+    print("1.Dune")
+    print("2.Dune part 2")
+    print("3.hytd")
+    print("4.Dragonheart")
+    print("5.Avatar")
+
+def top_Five_Movies(a, b, c, d, e):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+top_Five_Movies("dune", "dune part 2", "HYTD", "Dragonheart", "Avatar")
