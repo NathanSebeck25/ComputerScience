4ec9b7fbf51e761a91a32778b03aef40230c5668
NathanSebeck25 <pikana7e@gmail.com>
2024-09-30 14:19:37 -0500

    practice

---
+++ Unit 2 - Functions and Variables/Function-practice.py
@@ -0,0 +1,36 @@
+x = 5 
+
+def my_function():      #define the function with a name
+    print("hello world")
+    print("catdog")
+
+print("Owsoski r00lz d00d")
+
+def top_5_Movies():
+    print("Nathan's top 5 movies")
+    print("1.Dune")
+    print("2.Dune part 2")
+    print("3.hytd")
+    print("4.Dragonheart")
+    print("5.Avatar")
+
+top_5_Movies()
+
+#big curser mode ctrl alt down arrow
+
+def add(x, y):
+    print(x + y)
+
+add(5, 10)
+add(2, 2)
+add(15,15)
+add("cat", "dog")
+
+def top_Five_Movies(a, b, c, d, e):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+top_Five_Movies("dune", "dune part 2", "HYTD", "Dragonheart", "Avatar")
