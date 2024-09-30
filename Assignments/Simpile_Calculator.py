4acf7ff09d645ba7582288cc0335983ab430f29e
NathanSebeck25 <pikana7e@gmail.com>
2024-09-30 15:13:26 -0500

    work in progress 9-30 simpile calculator

---
+++ Assignments/Simpile_Calculator.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    print(x + y)
+    return x + y
+add (10, 7)
+
+def subtract(x, y):
+    print(x-y)
+    return x-y
+subtract (9, 6)
+
+def multiply(x, y):
+    print(x*y)
+    return x*y
+multiply (101.3, 5392)
+
+def devide(x, y):
+    print(x/ y)
+    return x / y 
+devide (10,2)
+
+def exponent(x, y):
+    print(x**y)
+    return x**y
