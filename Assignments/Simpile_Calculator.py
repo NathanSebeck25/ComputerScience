dd2a013205d59dc9e41b86368de3ce875df1e16d
NathanSebeck25 <pikana7e@gmail.com>
2024-10-01 11:26:29 -0500

    simpile calc up to modulus

--- Assignments/Simpile_Calculator.py
+++ Assignments/Simpile_Calculator.py
@@ -1,23 +1,39 @@
 def add(x,y):
     print(x + y)
     return x + y
-add (10, 7)
-
+a= int(input("Enter a x value\n:"))
+b= int(input("Enter a y value\n:"))
+add(a,b)
 def subtract(x, y):
     print(x-y)
     return x-y
-subtract (9, 6)
-
+c= int(input("Enter a x value\n:"))
+d= int(input("Enter a y value\n:"))
+subtract(c,d)
 def multiply(x, y):
     print(x*y)
     return x*y
-multiply (101.3, 5392)
-
+# multiply (101.3, 5392) what radio station is this?
+e= int(input("Enter a x value\n:"))
+f= int(input("Enter a y value\n:"))
+multiply(e,f)
 def devide(x, y):
     print(x/ y)
     return x / y 
-devide (10,2)
-
+g= int(input("Enter a x value\n:"))
+h= int(input("Enter a y value\n:"))
+devide(g,h)
 def exponent(x, y):
     print(x**y)
     return x**y
+i= int(input("Enter a x value\n:"))
+j= int(input("Enter a y value\n:"))
+exponent(i,j)
+
+def modulus(x,y):
+    print(x%y)
+    return x%y
+k=int(input(" Enter a x value\n:"))
+l=int(input(" Enter a y value\n:"))
+modulus(k,i)
+
