b5a6326c056bfe265ceb6d3f178dcdc37ee7ef05
NathanSebeck25 <pikana7e@gmail.com>
2024-10-03 15:07:00 -0500

    Unit 2 final Test

---
+++ Tests/unit2test.py
@@ -0,0 +1,35 @@
+# This is part one of the test 
+# this is a program that asks for three words that will be concatinated together 
+x = input("Type a word\n:")
+
+y = input("type a word\n:")
+
+legends_za = input("type a word\n:")
+
+print( x + y + legends_za)
+
+# This is part 2 of the test
+#This part of the code will take three peramiters and add them together 
+
+a = int(input("type an integer\n:"))
+b = int(input("type an integer\n:"))
+c = int(input("type an integer\n:"))
+
+def add_three():
+    print( a + b + c )
+    return a + b + c
+
+add_three()
+
+
+
+#this is #3 on the codeing part of the test 
+
+def data_three(g,u,d,s):
+    g = input("type a word\n:")
+    u = float(input("type a float\n:"))
+    d = int(input("type a integer\n"))
+    s = u+d
+    print(g+s)
+
+data_three()
