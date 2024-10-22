3b4b71b91841538d27d796ed9844460d45fcfe1e
NathanSebeck25 <pikana7e@gmail.com>
2024-10-22 11:26:20 -0500

    why did i make leg?

---
+++ Assignments/Text_adventure_project(LOL).py
@@ -0,0 +1,12 @@
+# Asking what lane they want to play 
+answer = input("What lane do you mant to play top, jng, mid, bot, sup?(please answer with one of the listed roles)\n:").lower
+#List of champs
+Ap      = "abillity bassed Champ"
+Tank    = "Hp or arrmor bassed Champ"
+Ad      = "Attack bassed champ"
+def Top_champs(Naut, Sett, Mord):
+    input("What champ are you going to play Naut, Sett, Mord(please answer with one of the listed camp in that fotmat)\n:").lower
+    Naut = Ap, Tank
+
+if answer = "top":
+    Top_champs
---
+++ Unit 3 Conditionals and Expresions/randomization.py
@@ -0,0 +1,13 @@
+import random
+
+r = random.randrange(0,100)
+#first number is inclusive, second is excluseve 
+print(r)
+
+print("You have a 25 percent chance to win!")
+p = random.random()
+if p<=0.25:
+    print('You win!')
+
+else:
+    print("womp womp, you lost!")
