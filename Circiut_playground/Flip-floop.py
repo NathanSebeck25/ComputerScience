a7e5157e4d09e3169fdd9860d1652df7ce619475
NathanSebeck25 <pikana7e@gmail.com>
2024-11-12 15:15:09 -0600

    flip-floop done

---
+++ Circiut_playground/Flip-floop.py
@@ -0,0 +1,23 @@
+from adafruit_circuitplayground import cp
+
+import time
+
+
+while True:
+   if cp.switch:
+      for z in range(5,10):
+         cp.pixels[z]= (0, 0, 0)
+            
+            
+      for i in range(0,5):
+            
+            cp.pixels[i]= (47, 2, 79)
+            
+   else:
+      for y in range(0,5):
+            
+            cp.pixels[y]= (0, 0, 0)
+            
+      for b in range(5,10):
+            
+            cp.pixels[b]= (47, 2, 79)
