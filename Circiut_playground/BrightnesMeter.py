c3d67b200289bbad52bce7dc4e55ca1915ebb662
NathanSebeck25 <pikana7e@gmail.com>
2024-11-19 15:09:56 -0600

    i ain't doin stuff again

---
+++ Circiut_playground/BrightnesMeter.py
@@ -0,0 +1,37 @@
+from adafruit_circuitplayground import cp
+
+import time
+
+import random
+
+def color_meter():
+    while cp.light < 30:
+            cp.pixels[0] = (0,0,5)
+    while cp.light < 27:
+            cp.pixels[1] = (0,0,5)
+    while cp.light < 24:
+            cp.pixels[2] = (0,0,5)  
+    while cp.light < 21:
+            cp.pixels[3] = (0,0,5)
+    while cp.light < 18:
+            cp.pixels[4] = (0,0,5)
+    while cp.light < 15:
+            cp.pixels[5] = (0,0,5)  
+    while cp.light < 12:
+            cp.pixels[6] = (0,0,5)
+    while cp.light < 9:
+            cp.pixels[7] = (0,0,5)
+    while cp.light < 6:
+            cp.pixels[8] = (0,0,5)  
+    while cp.light < 3:
+            cp.pixels[9] = (0,0,5)  
+
+            
+    
+while True:
+    if cp.button_a:
+        color_meter()
+   
+    
+    if cp.button_b:
+        cp.pixels.fill((0,0,0))
